0490f8d7cf419de8486c24f1085d33e5b8a296dd
RafaelWill <willekens.rafael@gmail.com>
2021-03-31 10:17:44 +0200

    Story 4 updateItem controller added

--- api/src/main/java/be/willekens/template/api/controllers/ItemController.java
+++ api/src/main/java/be/willekens/template/api/controllers/ItemController.java
@@ -2,6 +2,7 @@ package be.willekens.template.api.controllers;
 
 import be.willekens.template.api.dto.item.CreateItemDto;
 import be.willekens.template.api.dto.item.ItemDto;
+import be.willekens.template.api.dto.item.UpdateItemDto;
 import be.willekens.template.api.mappers.ItemMapper;
 import be.willekens.template.service.ItemService;
 import org.slf4j.Logger;
@@ -28,5 +29,12 @@ public class ItemController {
     public ItemDto addItem(@RequestHeader String authorizationId, @RequestBody CreateItemDto createItemDto) {
         logger.info("A user with id " + authorizationId + " is requesting permission to create a new item");
         return itemMapper.mapToDto(itemService.addItem(itemMapper.createItem(createItemDto),authorizationId));
+    }
+
+    @PostMapping(path = "/{itemId}", consumes = "application/json", produces = "application/json")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ItemDto updateItem(@RequestHeader String authorizationId, @PathVariable String itemId, @RequestBody UpdateItemDto updateItemDto) {
+        logger.info("A user with id " + authorizationId + " is requesting to update an item with id " + itemId);
+        return itemMapper.mapToDto(itemService.updateItem(itemId, updateItemDto));
     }
 }
